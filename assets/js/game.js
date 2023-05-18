cacc5b7bd664d8a91061775d7184882636349001
unknown <loltony79@gmail.com>
2023-05-18 12:52:24 -0400

    something

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,6 +22,7 @@ function rand(max) {
     return Math.floor(Math.random() * max);
 }
 
+
 // Generates the questions and the iframe
 function generate_questions(song, id) {
     // Setting up local variables
@@ -39,6 +40,7 @@ function generate_questions(song, id) {
     iframe.setAttribute("src", id_img[ran]);
     // The option array takes in the correct answer
     options.push(song_q[ran]);
+    var answer = song_q[ran];
     // Removes the question asked so it doesn't repeat
     song_q = song_q.filter(item => item != song_q[ran]);
     // Removes the link so it doesn't repeat
@@ -47,8 +49,16 @@ function generate_questions(song, id) {
     song_a = song_a.filter(item => item != song_a[ran]);
     // Concats the 2 array together 
     options = options.concat(generate_3questions(song_a));
+    options = randomize(options);
     for (var i = 0; i < 4; i ++) {
-        buttons[i].textContent = options[i];
+      buttons[i].textContent = options[i];
+    }
+    for (var i = 0; i < 4; i ++ ) {
+      if (buttons[i].textContent === answer) {
+        buttons[i].setAttribute("data-answer", "correct");
+      } else {
+        buttons[i].setAttribute("data-answer", "uncorrect");
+      }
     }
 }
 
@@ -64,12 +74,25 @@ function generate_3questions(answer) {
       song_a = song_a.filter(item => item != song_a[ran]);
     }
     return options;
+}
+
+function randomize(array) {
+  var sort = array;
+  var options = [];
+  for (var i = 0; i < array.length; i ++) {
+    var num = rand(sort.length);
+    options.push(sort[num]);
+    sort = sort.filter(item => item != sort[num]);
   }
+  return options;
+}
+
+generate_questions(songArray, song_links);
   
 
   
 
-//Append variables to HTML button
+// //Append variables to HTML button
 
 //Event Listener for Buttons
 answerA.addEventListener('click', function(event){
